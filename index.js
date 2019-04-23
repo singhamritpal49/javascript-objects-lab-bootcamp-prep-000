7d94cc0bae30d0393e239180bedb7710f953e68c
Amritpal Singh <singhamritpal49@gmail.com>
2019-04-23 14:22:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
-
 function deleteFromObjectByKey(object, key) {
-  delete {object: key} 
+  var newObj = Object.assign({},object);
+  delete newObj[key];
+  return newObj;
 }
